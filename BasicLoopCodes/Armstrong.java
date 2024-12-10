a5456430438cd3eb6714398df7624b852405b4f8
abhaypatel0110 <abhaypatel0118@gmail.com>
2024-12-10 23:48:31 +0530

    removed extra print statements from armstrong code

--- BasicLoopCodes/Armstrong.java
+++ BasicLoopCodes/Armstrong.java
@@ -8,17 +8,15 @@ public class Armstrong {
         System.out.println("Enter the number to check for armstrong");
         int number = input.nextInt();
         int numberLength = String.valueOf(Math.abs(number)).length();
-        System.out.print(numberLength+"is the ength of number");
         int reminder,checkNumber = 0;
         int number1=number;
         while(number1>0)
         {
             reminder = number1 % 10;
-            System.out.println("reminder is"+reminder);
             checkNumber = (int)Math.pow(reminder, numberLength) + checkNumber;
             number1 = number1/10; 
         }
-        System.out.println(checkNumber);
+        
         if(checkNumber==number)
         {
             System.out.println("Given number "+number+" is g number");
